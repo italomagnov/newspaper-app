21d772002610845e2a25c4b4276b204000ef7f09
Ítalo Magno <italomagnov@gmail.com>
2022-11-13 16:39:57 -0400

    iniciando o desafio Netshowme

---
+++ app/header.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className={styles.logo}>Newspaper</h1>
+        </div>
+    );
+};
+
+export default Header;
